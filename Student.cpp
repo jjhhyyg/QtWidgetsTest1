76a5bc7c2850be6d123352941e36f84f503fd5d1
hosealle <hosealle@outlook.com>
2022-03-17 14:10:33 +0800

    信号的传递

--- Student.cpp
+++ Student.cpp
@@ -7,13 +7,13 @@ Student::Student(QObject* parent):QObject(parent)
 
 void Student::treat() 
 {
-	qDebug() << QString::fromLocal8Bit("请老师吃饭");
+	qDebug() << QString::fromLocal8Bit("请老师吃饭").toUtf8().data();
 }
 
 void Student::treat(QString foodName)
 {
 	// QString默认有引号
-	qDebug() << QString::fromLocal8Bit("请老师吃") << foodName;
+	// qDebug() << QString::fromLocal8Bit("请老师吃") << foodName;
 	// 转换为char*就可以消除引号：先转成QByteArray，再转换为char*
 	qDebug() << QString::fromLocal8Bit("请老师吃").toUtf8().data() << foodName.toUtf8().data();
 }
--- myWidget.cpp
+++ myWidget.cpp
@@ -18,10 +18,30 @@ myWidget::myWidget(QWidget *parent)
     // 函数指针 -> 函数地址
     void(Teacher:: *teacherSignal)(QString) = &Teacher::hungry;
     void(Student:: *studentSlot)(QString) = &Student::treat;
-    connect(t, teacherSignal, s, studentSlot);
+    this->connect(t, teacherSignal, s, studentSlot);
 
     // 调用下课函数
-    afterClass();
+    // afterClass();
+
+    // 点击一个下课的按钮，再触发下课（有参）
+    QPushButton* btn = new QPushButton(QString::fromLocal8Bit("下课（有参）"),this);
+    this->resize(600, 400);
+    this->connect(btn,&QPushButton::clicked,this,&myWidget::afterClass);
+
+    // 点击一个下课的按钮，再触发下课（无参），信号触发信号
+    void(Teacher:: * teacherSignal2)(void) = &Teacher::hungry;
+    void(Student:: * studentSlot2)(void) = &Student::treat;
+    this->connect(t, teacherSignal2, s, studentSlot2);
+    QPushButton* btn2 = new QPushButton(QString::fromLocal8Bit("下课（无参）"), this);
+    btn2->move(200, 0);
+    this->connect(btn2, &QPushButton::clicked, t, teacherSignal2);
+
+    // 断开信号
+    this->disconnect(btn, &QPushButton::clicked, this, &myWidget::afterClass);
+
+    // 扩展：一个信号可以连接多个槽函数，多个信号可以连接同一个槽函数
+    // 信号和槽的参数类型必须一一对应
+    // 信号和槽的参数个数不一定一一对应，但槽的参数一定是信号的参数的子集
 }
 
 void myWidget::afterClass()
